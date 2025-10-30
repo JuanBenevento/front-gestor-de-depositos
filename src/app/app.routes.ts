0d818caee0b34b4e45f90b99eb93402056948ecf
JuanBenevento <juanmanuelbenevento@gmail.com>
2025-10-30 11:19:54 -0300

    Se agrega seccion para gestionar clientes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,7 +1,7 @@
 import { Routes } from '@angular/router';
 import { LoginComponent } from './features/login/login.component';
 import { DashboardComponent } from './features/dashboard/dashboard.component';
-import { UsuariosListComponent } from './features/gestion-usuarios/usuario-list/usuario-list.component';
+import { UsuarioListComponent } from './features/gestion-usuarios/usuario-list/usuario-list.component';
 import { UsuarioFormComponent } from './features/gestion-usuarios/usuario-form/usuario-form.component';
 import { AuthGuard } from './core/guards/auth.guard';
 import { RoleGuard } from './core/guards/role.guard';
@@ -9,6 +9,10 @@ import { ZonaListComponent } from './features/gestion-zonas/zona-list/zona-list.
 import { ZonaFormComponent } from './features/gestion-zonas/zona-form/zona-form.component';
 import { UbicacionListComponent } from './features/gestion-ubicaciones/ubicacion-list/ubicacion-list.component';
 import { UbicacionFormComponent } from './features/gestion-ubicaciones/ubicacion-form/ubicacion-form.component';
+import { ProveedoresListComponent } from './features/gestion-proveedores/proveedores-list/proveedores-list.component';
+import { ProveedoresFormComponent } from './features/gestion-proveedores/proveedores-form/proveedores-form.component';
+import { ClienteListComponent } from './features/gestion-clientes/cliente-list/cliente-list.component';
+import { ClienteFormComponent } from './features/gestion-clientes/cliente-form/cliente-form.component';
 
 export const routes: Routes = [
   { path: '', redirectTo: 'login', pathMatch: 'full' },
@@ -29,7 +33,13 @@ export const routes: Routes = [
       { path: 'zonas/editar/:id', component: ZonaFormComponent, canActivate: [RoleGuard('ADMIN')] },
       { path: 'ubicaciones', component: UbicacionListComponent, canActivate: [RoleGuard('ADMIN')] },
       { path: 'ubicaciones/nuevo', component: UbicacionFormComponent, canActivate: [RoleGuard('ADMIN')] },
-      { path: 'ubicaciones/editar/:id', component: UbicacionFormComponent, canActivate: [RoleGuard('ADMIN')] }
+      { path: 'ubicaciones/editar/:id', component: UbicacionFormComponent, canActivate: [RoleGuard('ADMIN')] },
+      { path: 'proveedores', component: ProveedoresListComponent, canActivate: [RoleGuard('ADMIN')] },
+      { path: 'proveedores/nuevo', component: ProveedoresFormComponent, canActivate: [RoleGuard('ADMIN')] },
+      { path: 'proveedores/editar/:id', component: ProveedoresFormComponent, canActivate: [RoleGuard('ADMIN')] },
+      { path: 'clientes', component: ClienteListComponent, canActivate: [RoleGuard('ADMIN')] },
+      { path: 'clientes/nuevo', component: ClienteFormComponent, canActivate: [RoleGuard('ADMIN')] },
+      { path: 'clientes/editar/:id', component: ClienteFormComponent, canActivate: [RoleGuard('ADMIN')] }
     ]
   },
 
